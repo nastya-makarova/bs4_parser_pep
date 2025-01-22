0a3ac3e16dad5bb63a1df8b03a26bce31f492271
Nastya Makarova <a.o.konovalova@gmail.com>
2025-01-22 11:11:03 +0400

    Working version for searching status and type on the pep page.

--- src/pep.py
+++ src/pep.py
@@ -25,14 +25,29 @@ for table in tables:
     link_tags.extend(table.find_all('a', {'class': 'pep reference internal'}))
 links = link_tags[::2]
 
-for link in links:
-    pep_url = urljoin(PEP_DOC_URL, link['href'])
-    response = session.get(pep_url)
-    response.encoding = 'utf-8'
-    soup = BeautifulSoup(response.text, 'lxml')
-    table = soup.find('dl')
-    status = table.find('dd', {'title': 'Currently valid informational guidance, or an in-use process'})
-    print(status)
+# for link in links:
+#     pep_url = urljoin(PEP_DOC_URL, link['href'])
+#     response = session.get(pep_url)
+#     response.encoding = 'utf-8'
+#     soup = BeautifulSoup(response.text, 'lxml')
+#     table = soup.find('dl')
+#     print(table)
+#     # status = table.find('dd', {'title': 'Currently valid informational guidance, or an in-use process'})
+#     # print(status)
 
-
-
+link = links[0]
+pep_url = urljoin(PEP_DOC_URL, link['href'])
+response = session.get(pep_url)
+response.encoding = 'utf-8'
+soup = BeautifulSoup(response.text, 'lxml')
+table = soup.find('dl')
+tags_dt = table.find_all('dt')
+print(tags_dt)
+for tag in tags_dt:
+    if tag.text == 'Status:':
+        status_pep = tag.find_next_sibling().text
+        print(status_pep)
+    
+    if tag.text == 'Type:':
+        type_pep = tag.find_next_sibling().text
+        print(type_pep)
